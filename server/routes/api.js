793ac56ddc2fd949c4ad4a9cb95313dd6c6e0b93
ethan <ethanmannette@gmail.com>
2015-09-11 10:02:52 -0600

    added new post request. Added database.js file.

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,4 +5,9 @@ router.get('/superheros', function(req, res, next) {
   res.render('api', { title: 'Superhero API' });
 });
 
+router.post('/superheros', function(req, res, next) {
+  console.log(req.body.name);
+  res.redirect('/api/superheros');
+});
+
 module.exports = router;
